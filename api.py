8cfb34862fe776422e17727dd822b5677e1522af
HuyKevin09 <90472729+HuyKevin09@users.noreply.github.com>
2023-05-27 11:58:13 +0200

    Update api.py

--- api.py
+++ api.py
@@ -12,7 +12,8 @@ def result():
     lat = request.args.get('lat')
     lon = request.args.get('lon')
     # On recupere la variable d'environnement "API_KEY_ definie dans le Dockerfile
-    api_key = os.environ.get("API_KEY")
+    # api_key = os.environ.get("API_KEY")
+    api_key = "a1e0149e963250ea84c1f39140dcc205"
     url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric'
     response = requests.get(url)
     return response.json()
